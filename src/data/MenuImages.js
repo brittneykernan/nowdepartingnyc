4d8e5d7fa535f8c81045837cce214776345593c9
brittneykernan <misskernan@gmail.com>
2023-11-01 18:43:35 -0400

    changing typography to follow golden rule

---
+++ src/data/MenuImages.js
@@ -0,0 +1,97 @@
+import barcSrc from "../assets/theodor-vasile-LSscVPEyQpI-unsplash.jpg";
+import rioSrc from "../assets/sebastien-goldberg-Q8KGwGdDsW8-unsplash.jpg";
+import iceSrc from "../assets/jonny-auh-z99iWocuDt0-unsplash.jpg";
+import costaSrc from "../assets/selina-bubendorfer-X3zFdLVQLKE-unsplash.jpg";
+import swissSrc from "../assets/bhumil-chheda--h45iGpkkPA-unsplash.jpg";
+import parSrc from "../assets/thibault-penin-zj_zOAaT8as-unsplash.jpg";
+import milSrc from "../assets/julia-solonina-lZKMFhsPQ30-unsplash.jpg";
+import willSrc from "../assets/linmiao-xu-60qA6VQGeyA-unsplash.jpg";
+import sagSrc from "../assets/duncan-kidd-_w0ETDhqEBc-unsplash.jpg";
+import florSrc from "../assets/gabriela-testa-8Y6-Ewhp2g8-unsplash.jpg";
+
+const barcImage = {
+	src: barcSrc,
+	artistName: "Theodor Vasile",
+	url: "https://test.com",
+    alt: "Casa Battló by Theodor Vasile"
+};
+
+const rioImage = {
+	src: rioSrc,
+	artistName: "Sébastien Goldberg",
+	url: "",
+    alt: "Rio de Janeiro by Sébastien Goldberg"
+};
+
+const iceImage = {
+    src: iceSrc,
+    artistName: "Jonny Auh",
+    url: "",
+    alt: "Iceland by Jonny Auh"
+};
+
+const costaImage = {
+    src: costaSrc,
+    artistName: "Selina Bubendorfer",
+    url: "",
+    alt: "La Fortuna by Selina Bubendorfer"
+};
+
+const swissImage = {
+    src: swissSrc,
+    artistName: "Bhumil Chheda",
+    url: "",
+    alt: "Switzerland by Bhumil Chheda"
+};
+
+const parImage = {
+    src: parSrc,
+    artistName: "Thibault Penin",
+    url: "",
+    alt: "Paris by Thibault Penin"
+};
+
+const milImage = {
+    src: milSrc,
+    artistName: "Julia Solonina",
+    url: "",
+    alt: "Milan by Julia Solonina"
+};
+
+const willImage = {
+    src: willSrc,
+    artistName: "Linmiao Xu",
+    url: "",
+    alt: "Brooklyn by Linmiao Xu"
+};
+
+const sagImage = {
+    src: sagSrc,
+    artistName: "Duncan Kidd",
+    url: "",
+    alt: "Sagrada Familia by Duncan Kidd"
+};
+
+const florImage = {
+    src: florSrc,
+    artistName: "Gabriela Testa",
+    url: "",
+    alt: "Florida by Gabriela Testa"
+};
+
+export const LocationMenuImages = {
+    barcImage,
+    rioImage,
+    iceImage,
+    costaImage,
+    swissImage,
+    parImage,
+    milImage,
+    willImage
+}
+
+export const CategoryMenuImages = {
+    sagImage,
+    florImage
+}
+
---
+++ src/data/MenuItems.js
@@ -0,0 +1,67 @@
+import { LocationMenuImages } from "./MenuImages";
+
+const barcelona = {
+    title: "Barcelona",
+    slug: "guide",
+    image: LocationMenuImages.barcImage,
+};
+
+const rio = {
+    title: "Rio de Janeiro",
+    slug: "rio",
+    image: LocationMenuImages.rioImage,
+    comingSoon: true,
+};
+
+const iceland = {
+    title: "Iceland",
+    slug: "iceland",
+    image: LocationMenuImages.iceImage,
+    comingSoon: true,
+}
+
+const paris = {
+    title: "Paris",
+    slug: "paris",
+    image: LocationMenuImages.parImage,
+    comingSoon: true,
+}
+
+const northWilliamsburg = {
+    title: "North Williamsburg",
+    slug: "north-williamsburg",
+    image: LocationMenuImages.willImage,
+    comingSoon: true,
+}
+
+const costaRica = {
+    title: "Costa Rica",
+    slug: "costa-rica",
+    image: LocationMenuImages.costaImage,
+    comingSoon: true,
+}
+
+const milan = {
+    title: "Milan",
+    slug: "milan",
+    image: LocationMenuImages.milImage,
+    comingSoon: true,
+}
+
+const switzerland = {
+    title: "Switzerland",
+    slug: "switzerland",
+    image: LocationMenuImages.swissImage,
+    comingSoon: true,
+}
+
+export const LocationMenuItems = [
+    barcelona,
+    rio,
+    iceland,
+    paris,
+    northWilliamsburg,
+    costaRica,
+    milan,
+    switzerland
+];
