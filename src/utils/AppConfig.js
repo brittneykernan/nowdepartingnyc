d783dfd20b75cb31b941d629bce012a24b34113f
brittneykernan <misskernan@gmail.com>
2023-12-30 19:10:17 -0500

    styling right rail

---
+++ src/utils/AppConfig.js
@@ -0,0 +1,11 @@
+export const AppConfig = {
+  domain: 'nowdeparting.nyc',
+  site_name: 'Now Departing NYC',
+  title: 'Now Departing NYC',
+  description:
+    '',
+  author: 'Brittney Kernan',
+  email: 'brittneykernan@gmail.com',
+  locale_region: 'en-us',
+  locale: 'en',
+};
